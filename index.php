87a8a552773fedea4e332d43bfa639d1fc6db05c
Antonio Góngora <antonio.gongora1491@gmail.com>
2019-04-02 17:56:58 -0600

    Archivos base: Landing Page Salta

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Congreso Parques Sudámerica</title>
+    <?php require("inc/head.php"); ?>
+  </head>
+  <body>
+    <main>
+      <header id="portada" class="contenedorPortada">
+        <div class="itemContenidoPortada">
+          <div class="fecha">
+            <h4 id="">6-9 Octubre 2019</h4>
+            <h5>provincia de salta - Argentina</h5>
+          </div>
+        </div>
+        <div class="itemContenidoPortada">
+          <figure class="logotipo">
+            <img src="img/congreso_parques_sudamerica.png" alt="Congreso Parques Sudamérica">
+          </figure>
+        </div>
+        <div class="itemContenidoPortada">
+
+        </div>
+        <div class="itemContenidoPortada">
+
+        </div>
+      </header>
+      <section id="convocatoria" >
+
+      </section>
+      <footer>
+        <?php include "inc/footer.php" ?>
+      </footer>
+    </main>
+  </body>
+</html>
